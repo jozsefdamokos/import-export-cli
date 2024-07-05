945606900f0217faa918a572a7bcf38b5de45dcd
Jozsef Damokos <j.damokos@shopware.com>
2024-07-05 16:35:48 +0300

    Refactor to mimic rust tool's approach

--- src/Command/NewImportExportCommand.php
+++ src/Command/NewImportExportCommand.php
@@ -2,12 +2,14 @@
 
 namespace ImportExport\Command;
 
-use ImportExport\Deserializer\PriceDeserializer;
-use ImportExport\Deserializer\StringDeserializer;
+use Shopware\Core\Framework\Uuid\Uuid;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Twig\Environment;
+use Twig\Loader\ArrayLoader;
 
 #[AsCommand(
     name: 'import-export:run',
@@ -15,217 +17,233 @@ use Symfony\Component\Console\Output\OutputInterface;
 )]
 class NewImportExportCommand extends Command
 {
-    /**
-     * @var array<string, class-string>
-     */
-    private array $deserializers = [
-        'uuid' => StringDeserializer::class,
-        'string' => StringDeserializer::class,
-        'boolean' => StringDeserializer::class,
-        'int' => StringDeserializer::class,
-        'float' => StringDeserializer::class,
-        'price' => PriceDeserializer::class,
-    ];
-
-    private array $profile = [
-        'entity' => 'product',
-        'mappings' => [
-            [
-                'header' => 'Product ID',
-                'path' => 'id',
-                'type' => 'uuid',
-            ],
-            [
-                'header' => 'Product number',
-                'path' => 'productNumber',
-                'type' => 'string',
-            ],
-            [
-                'header' => 'Product name',
-                'path' => 'translations.ENG.name',
-                'type' => 'string',
-            ],
-            [
-                'header' => 'Active',
-                'path' => 'active',
-                'type' => 'boolean',
-            ],
-            [
-                'header' => 'Stock',
-                'path' => 'stock',
-                'type' => 'int',
-            ],
-            [
-                'header' => 'Category',
-                'path' => 'categories.name',
-                'type' => 'string',
-            ],
-            [
-                'header' => 'Manufacturer',
-                'path' => 'manufacturer.translations.ENG.name',
-                'type' => 'string',
-            ],
-            [
-                'header' => 'Delivery time',
-                'path' => 'deliveryTime.translations.ENG.name',
-                'type' => 'string',
-            ],
-            [
-                'header' => 'Tax rate',
-                'path' => 'tax.taxRate',
-                'type' => 'float',
-            ],
-            [
-                'header' => 'Price EUR (NET)',
-                'path' => 'price.EUR.net',
-                'type' => 'float',
-            ],
-            // [
-            //     'pathMapping' => [
-            //         'name' => 'Property name',
-            //         'position' => 'Property position',
-            //     ],
-            //     'type' => 'custom',
-            //     'path' => 'properties',
-            //     'deserializer' => 'ImportExport\Deserializer\CustomPropertyDeserializer',
-            // ],
-            // [
-            //     'header' => 'Tags',
-            //     'type' => 'custom',
-            //     'path' => 'tags',
-            //     'deserializer' => 'ImportExport\Deserializer\CustomTagDeserializer',
-            // ],
-            // [
-            //     'header' => 'Product name',
-            //     'path' => 'name',
-            //     'type' => 'string',
-            // ],
-            // [
-            //     'pathMapping' => [
-            //         'EUR.net' => 'Price EUR (NET)',
-            //         'EUR.gross' => 'Price EUR (GROSS)',
-            //         'USD.net' => 'Price USD (NET)',
-            //         'USD.gross' => 'Price USD (GROSS)'
-            //     ],
-            //     'type' => 'custom',
-            //     'path' => 'price',
-            //     'deserializer' => 'ImportExport\Deserializer\CustomPriceDeserializer',
-            // ],
-            // [
-            //     'pathMapping' => [
-            //         'name' => ['Tag1', 'Tag2', 'Tag3'],
-            //     ],
-            //     'type' => 'custom',
-            //     'path' => 'tags',
-            //     'deserializer' => 'ImportExport\Deserializer\CustomTagsDeserializer',
-            // ],
-            // [
-            //     'pathMapping' => [
-            //         'EUR.net' => 'Price EUR (NET)',
-            //         'EUR.gross' => 'Price EUR (GROSS)',
-            //         'USD.net' => 'Price USD (NET)',
-            //         'USD.gross' => 'Price USD (GROSS)'
-            //     ],
-            //     'type' => 'price',
-            //     'path' => 'price',
-            // ],
-            // [
-            //     'pathMapping' => [
-            //
-            //     ],
-            //     'type' => 'custom',
-            //     'path' => 'custom.whatever',
-            //     'deserializer' => 'ImportExport\Deserializer\CustomPriceDeserializer',
-            // ],
-            // [
-            //     'header' => 'Test header',
-            //     'type' => 'custom',
-            //     'path' => 'custom.whatever',
-            //     'deserializer' => 'ImportExport\Deserializer\CustomPriceDeserializer',
-            // ],
-        ],
-    ];
+    protected function configure(): void
+    {
+        $this->addArgument('type', InputArgument::REQUIRED, 'Import/Export type.');
+    }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        // get csv from path
-        $file = fopen(__DIR__ . '/../../products-to-validate.csv', 'r');
+        // TODO Move the selection of profile into command argument
+        $yaml = file_get_contents(__DIR__ . '/../../profiles/product_required.yaml');
 
-        $csv = [];
+        $profile = yaml_parse($yaml);
 
-        // read headers of csv then create associative array
+        $this->validateCsvAgainstSchema($profile);
+
+        if ($input->getArgument('type') === 'export') {
+            $this->export($profile);
+
+            return Command::SUCCESS;
+        }
+
+        if ($input->getArgument('type') === 'import') {
+            $this->import($profile);
+
+            return Command::SUCCESS;
+        }
+
+        return Command::FAILURE;
+    }
+
+    private function export(array $profile): void
+    {
+        $serializeScript = $profile['serialize_script'];
+
+        $loader = new ArrayLoader([
+            'serializeScript' => $serializeScript,
+        ]);
+
+        $twig = new Environment($loader);
+
+        $entities = $this->getEntities($profile)['data'];
+
+        $stream = fopen(__DIR__ . '/../../export.csv', 'w+');
+
+        // add headers from profile mappings
+        $headers = array_map(fn($mapping) => $mapping['file_column'], $profile['mappings']);
+
+        fputcsv($stream, $headers);
+
+        foreach ($entities as $entity) {
+            $row = [];
+
+            foreach ($profile['mappings'] as $mapping) {
+                if (isset($mapping['entity_path'])) {
+                    // explode entity path to get potential nested values
+                    $entityPath = explode('.', $mapping['entity_path']);
+
+                    // if entity path has only one element, get the value directly
+                    if (count($entityPath) === 1) {
+                        $row[$mapping['entity_path']] = $entity[$mapping['entity_path']];
+
+                        continue;
+                    }
+
+                    // if entity path has more than one element, get the nested value
+                    $nestedValue = $entity;
+                    foreach ($entityPath as $path) {
+                        $nestedValue = $nestedValue[$path];
+                    }
+
+                    $row[$mapping['entity_path']] = $nestedValue;
+
+                    continue;
+                }
+
+                // set empty value for key which should later be filled by the serialize script
+                if (isset($mapping['key'])) {
+                    $row[$mapping['key']] = '';
+                }
+            }
+
+            $twig->render('serializeScript', [
+                'entity' => $entity,
+                'row' => &$row,
+            ]);
+
+            fputcsv($stream, $row);
+        }
+
+        fclose($stream);
+    }
+
+    private function import(array $profile): void
+    {
+        // TODO get csv via command argument
+        $file = fopen(__DIR__ . '/../../export.csv', 'r');
+
         $headers = fgetcsv($file);
 
+        $csv = [];
         while ($row = fgetcsv($file)) {
             $csv[] = array_combine($headers, $row);
         }
 
-        // deserialize csv data
-        $deserialized = [];
-
-        $this->validateCsvAgainstSchema();
-
-        foreach ($csv as $row) {
-            $deserializedRow = [];
-
-            foreach ($this->profile['mappings'] as $mapping) {
-                if ($mapping['type'] === 'custom') {
-
-                    $deserializer = new $mapping['deserializer']();
-
-                    if (isset($mapping['pathMapping'])) {
-                        $value = [];
-                        foreach ($mapping['pathMapping'] as $key => $header) {
-                            if (\is_array($header)) {
-                                $value[$key] = [];
-                                foreach ($header as $headerKey => $headerValue) {
-                                    $value[$key][$headerKey] = $row[$headerValue];
-                                }
-
-                                continue;
-                            }
-
-                            $value[$key] = $row[$header];
-                        }
-
-                        $value = $deserializer->deserialize($value);
-                    } else {
-                        $value = $row[$mapping['header']];
-
-                        $value = $deserializer->deserialize($value);
-                    }
-
-                    $deserializedRow = array_merge_recursive($deserializedRow, $value);
-                } else {
-                    $deserializer = new $this->deserializers[$mapping['type']]();
-
-                    $value = $row[$mapping['header']];
-
-                    $value = $deserializer->deserialize($value);
-
-                    $deserializedRow[$mapping['path']] = $value;
-                }
-            }
-
-            $deserialized[] = $deserializedRow;
+        // fetch array keys from profile mappings
+        $keys = [];
+        foreach ($profile['mappings'] as $mapping) {
+            $keys[$mapping['file_column']] = $mapping['entity_path'] ?? $mapping['key'];
         }
 
-        dd($deserialized);
+        // replace keys in csv with keys from profile mappings
+        foreach ($csv as &$row) {
+            $row = array_combine(array_map(fn($key) => $keys[$key], array_keys($row)), $row);
+        }
+
+        $deserializeScript = $profile['deserialize_script'];
+
+        $loader = new ArrayLoader([
+            'deserializeScript' => $deserializeScript,
+        ]);
+
+        $twig = new Environment($loader);
+
+        $payload = [];
+        foreach ($csv as $row) {
+            $entity = [];
+
+            foreach ($row as $key => $value) {
+                $mapping = $this->findMappingByEntityPath($key, $profile['mappings']);
+
+                if ($mapping === null) {
+                    continue;
+                }
+
+                // if not nested, set value directly
+                if (!str_contains($mapping['entity_path'], '.')) {
+                    $entity[$mapping['entity_path']] = $value;
+
+                    continue;
+                }
+
+                // if nested, set value in nested array
+                $entityPath = explode('.', $mapping['entity_path']);
+
+                $nestedEntity = &$entity;
+                foreach ($entityPath as $path) {
+                    if (!isset($nestedEntity[$path])) {
+                        $nestedEntity[$path] = [];
+                    }
+
+                    $nestedEntity = &$nestedEntity[$path];
+                }
+
+                $nestedEntity = $value;
+            }
+
+            $twig->render('deserializeScript', [
+                'entity' => &$entity,
+                'row' => $row,
+            ]);
+
+            $payload[] = $entity;
+        }
+
+        // TODO send payload to the API
+        dd($payload);
     }
 
-    private function validateCsvAgainstSchema(): void
+    /**
+     * TODO do this via API
+     */
+    private function getEntities(array $profile): array
     {
-        // get entity schema json
+        $manufacturer = [
+            'id' => Uuid::randomHex(),
+            'name' => 'Test Manufacturer',
+        ];
+
+        $euroCurrencyId = 'b7d2554b0ce847cd82f3ac9bd1c0dfca';
+        $usdCurrencyId = 'b7d2554b0ce847cd82f3ac9bd1c0dfcb';
+
+        // create 5 mock product entities
+        for ($i = 1; $i <= 5; $i++) {
+            $netPrice = 10.00 * $i;
+            $grossPrice = 15.00 * $i;
+            $stock = rand(1, 100);
+
+            $product = [
+                'id' => Uuid::randomHex(),
+                'productNumber' => 'SW0000' . $i,
+                'name' => 'Test Product ' . $i,
+                'stock' => $stock,
+                'taxId' => Uuid::randomHex(),
+                'price' => [
+                    [
+                        'currencyId' => $usdCurrencyId,
+                        'net' => $netPrice,
+                        'gross' => $grossPrice,
+                        'linked' => false,
+                    ],
+                    [
+                        'currencyId' => $euroCurrencyId,
+                        'net' => $netPrice * 0.9,
+                        'gross' => $grossPrice * 0.9,
+                        'linked' => false,
+                    ],
+                ],
+                'manufacturer' => [
+                    'id' => $manufacturer['id'],
+                    'name' => $manufacturer['name'],
+                ],
+            ];
+
+            $entities[] = $product;
+        }
+
+        return [
+            'data' => $entities,
+        ];
+    }
+
+    private function validateCsvAgainstSchema(array $profile): void
+    {
         $schema = file_get_contents(__DIR__ . '/../../../../../src/Administration/Resources/app/administration/test/_mocks_/entity-schema.json');
 
-        // json decode schema
         $schema = json_decode($schema, true);
 
-        // validate csv against schema
-
-        $entity = $this->profile['entity'];
-
-        $this->checkPathsForEntity($entity, $this->profile['mappings'], $schema);
+        $this->checkPathsForEntity($profile['entity'], $profile['mappings'], $schema);
     }
 
 
@@ -237,60 +255,47 @@ class NewImportExportCommand extends Command
         }
 
         foreach ($mappings as $mapping) {
-            if ($mapping['type'] === 'custom') {
+            if (!isset($mapping['entity_path'])) {
                 continue;
             }
 
-            $path = $mapping['path'];
-
+            $path = $mapping['entity_path'];
             $parts = explode('.', $path);
+            $rootPath = $parts[0];
 
-            // check if first path exists in schema
-            if (!isset($schema[$entityName]['properties'][$parts[0]])) {
+            // check if root path exists in schema
+            if (!isset($schema[$entityName]['properties'][$rootPath])) {
                 throw new \RuntimeException(sprintf('Path %s not found in %s.', $path, $entityName));
             }
 
-            if (count($parts) === 1) {
-                // check if type matches schema type
-                if ($schema[$entityName]['properties'][$parts[0]]['type'] !== $mapping['type']) {
-                    throw new \RuntimeException(sprintf(
-                        'Type %s does not match schema type %s for %s in %s',
-                        $mapping['type'],
-                        $schema[$entityName]['properties'][$parts[0]]['type'],
-                        $parts[0],
-                        $entityName
-                    ));
-                }
-            } else {
-                // if its multiple parts it should be an association or json
-                if ($schema[$entityName]['properties'][$parts[0]]['type'] !== 'association' && $schema[$entityName]['properties'][$parts[0]]['type'] !== 'json_object') {
+            if (count($parts) > 1) {
+                // if its multiple parts it should be an association
+                if ($schema[$entityName]['properties'][$rootPath]['type'] !== 'association') {
                     throw new \RuntimeException(sprintf('Path %s in %s is not an association.', $path, $entityName));
-                }
-
-                // we skip json here because we don't know the structure of it, so we cannot validate
-                if ($schema[$entityName]['properties'][$parts[0]]['type'] === 'json_object') {
-                    continue;
                 }
 
                 $paths = array_slice($parts, 1);
 
-                // we skip the language iso code for translations
-                if ($parts[0] === 'translations') {
-                    // todo validate that it's a valid iso code
-
-                    $paths = array_slice($paths, 1);
-                }
-
-                $entity = $schema[$entityName]['properties'][$parts[0]]['entity'];
+                $entity = $schema[$entityName]['properties'][$rootPath]['entity'];
                 $path = implode('.', $paths);
 
                 $mapping = [
-                    'path' => $path,
-                    'type' => $mapping['type'],
+                    'entity_path' => $path,
                 ];
 
                 $this->checkPathsForEntity($entity, [$mapping], $schema);
             }
         }
+    }
+
+    private function findMappingByEntityPath(string $entityPath, array $mappings): ?array
+    {
+        foreach ($mappings as $mapping) {
+            if (isset($mapping['entity_path']) && $mapping['entity_path'] === $entityPath) {
+                return $mapping;
+            }
+        }
+
+        return null;
     }
 }
--- src/Deserializer/Deserializer.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace ImportExport\Deserializer;
-
-interface Deserializer
-{
-    public function deserialize($data): mixed;
-}
